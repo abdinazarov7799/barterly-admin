9b459211478161c7bed70b81fed4b2204aa8c05a
abdinazarov7799 <parolxato@gmail.com>
2024-08-02 00:52:29 +0500

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,8 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port: 80}
+  server: {
+    host: '38.242.148.205',
+    port: 80
+  }
 })
